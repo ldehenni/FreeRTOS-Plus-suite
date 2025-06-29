807b6ccaf8b4629336284c72d48729c6d577db92
Louis Dehennin <louis.dehennin@iba-group.com>
2025-06-29 19:14:52 +0200

    Blinking LED.

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+#include "stm32f767xx.h"
+#include "gpio.h"
 
 int main()
 {
+	HAL_Init();
+	MX_GPIO_Init();
+
+	while(1)
+	{
+		HAL_Delay(1000);
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+	}
+
 	return 0;
 }
